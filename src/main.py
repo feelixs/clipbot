545036a693d19050f74c1951c3579a5d90a31552
Michael Felix <mikemh@uri.edu>
2024-07-17 11:25:56 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(level
 logger = logging.getLogger("main.py")
 DB = Database(maxsize=50, pool_name="CLYPPY_POOL")
 
-TWITCH_API = TwitchAPI(key=TwitchCreds.id, secret=TwitchCreds.secret, logger=logger, log_path=os.getenv('TWITCH_API_REMAINING_LOG'), log_name="twitch-api-all-usage-shard"+str(SHARD_ID)+".log")
+TWITCH_API = TwitchAPI(key=TwitchCreds.id, secret=TwitchCreds.secret, logger=logger, log_path=os.path.dirname(os.path.abspath(__file__)), log_name="twitch-api-all-usage-shard"+str(SHARD_ID)+".log")
 TWITCH_TOOLS = TwitchTools(TWITCH_API)
 DISCORD_TOOLS = DiscordTools(Bot, DB, logger, twitchtools=TWITCH_TOOLS)
 
